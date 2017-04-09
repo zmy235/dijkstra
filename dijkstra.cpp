90998d2d0c352d9a07b7632df2f02a5afe9212b4
zmy235 <zmy235@qq.com>
2017-04-09 11:07:31 +0800

    2017-4-9

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -1,145 +1,81 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <math.h>
-#include <iostream>
+/*
+*Dijkstra 纯手写
+*
+*2017-4-9 调试完成
+*
+*By veizi
+*/
+#include <bits/stdc++.h>
+#define INF 0x0FFFFFFF
 using namespace std;
-// 最短路径算法 Shortest Path Algorithm SPA 解决 最短路径 问题 by hsluoyz 
 
-//object part
-#define pointNumber 10
-#define distFT_MAX 1000000
-int distFT[pointNumber][pointNumber];
-//object part
-
-//point part
-
-int prev_[pointNumber];
-int dist[pointNumber];
-int s[pointNumber];
-//point part
-
-int RandomIntLessThan(int aint)
+int w[6][6]=
 {
-	return (int) ((double) aint * (double) rand() / (double) (RAND_MAX + 1));
-}
+    0,2,INF,4,INF,INF,
+    2,0,8,INF,1,INF,
+    INF,8,0,4,3,8,
+    4,INF,4,0,5,INF,
+    INF,1,3,5,0,10,
+    INF,INF,8,INF,10,0
+};
 
-int RandomTrueBy(double aprob)
+int main()
 {
-	double res = (double) rand() / (RAND_MAX + 1);
-	if (res <= aprob)
-		return 1;
-	else
-		return 0;
-}
+    int start;
+    cin>>start;
+    bool know[6]= {false}; //判断是否可以确定该点的Path
+    int cost[6]= {INF,INF,INF,INF,INF,INF}; //起点到该点的费用
+    int path[6]= {-1}; //当前点的前一个出发点
+    know[start]=true;
+    cost[start]=0;
+    path[start]=-1;
 
-void Dijkstra(int v)
-{
+    for(int i=0; i<6; i++)
+    {
+        cost[i] = w[start][i];
+        if(i!=start && cost[i]!=INF)
+            path[i] = start;
+    }
 
-	for (int i = 0; i < pointNumber; i ++)
-	{
-		dist[i] = distFT[v][i];
-		if (dist[i] == distFT_MAX)
-			prev_[i] = -1;
-		else
-			prev_[i] = 0;
-		s[i] = 0;
-	}
-	s[v] = 1;
+    int min_node=start;//当前未访问节点中最近的节点
 
-	for (int i = 0; i < pointNumber - 1; i ++)
-	{
-		int u = -1;
-		int tempdist = distFT_MAX;
-		for (int m = 0; m < pointNumber; m ++)
-		{
-			if (dist[m] < tempdist && s[m] == 0)
-			{
-				tempdist = dist[m];
-				u = m;
-			}
-		}
-		if (u == -1)
-		{
-			cout << "The isolated point exsits." << endl;
-			break;
-		}
-		s[u] = 1;
-		cout << "change:" << u << endl;
+    int unknown=4;//还不能确定路径的节点个数
+    while(unknown>0)
+    {
+        int min_cost=INF;//从起点到达 当前未访问最近点 的消耗
+        for(int j = 0; j < 6; j++)
+        {
+            if( !know[j] && (cost[j]<min_cost) )
+            {
+                min_cost = cost[j];
+                min_node = j;
+            }
+        }
 
-		for (int j = 0; j < pointNumber; j ++)
-		{
-			if (s[j] == 0)
-			{
-				int newdist = dist[u] + distFT[u][j];
-				if (newdist < dist[j])
-				{
-					dist[j] = newdist;
-					prev_[j] = u;
-				}
-			}
-		}
-	}
-}
+        know[min_node] = true;//标记 查找到的最近点k 为可以确定路径
+        unknown--;
 
-void DisplayMap(){
-	for (int i = 0; i < pointNumber; i ++)
-	{
-		for (int j = 0; j < pointNumber; j ++)
-		{
-			cout << distFT[i][j] << " ";
-		}
-		cout << endl;
-	}
-}
+        for(int j = 0; j < 6; j++)
+        {
+            if(!know[j] && min_cost+w[min_node][j] < cost[j])
+            {
+                cost[j] = min_cost+w[min_node][j];
+                path[j] = min_node;
+            }
+        }
+    }
 
-void Display(int v, int a)
-{
-	//point part
-	int stack[100];
-	int stackNumber = 0;
-	int i = a;
-	while (i != v)
-	{
-		stack[stackNumber] = i;
-		stackNumber ++;
-		i = prev_[i];
-	}
-	stack[stackNumber] = v;
-	stackNumber ++;
-	for (int j = stackNumber - 1; j > 0; j --)
-	{
-		cout << stack[j] << " -> ";
-	}
-	cout << stack[0] << endl;
-	//point part
-}
-
-void main(){
-	srand((unsigned) time(NULL));
-	//object part
-	for (int i = 0; i < pointNumber; i ++)
-	{
-		distFT[i][i] = 0;
-		for (int j = i + 1; j < pointNumber; j ++)
-		{
-			if (RandomTrueBy(0.1))
-			{
-				distFT[i][j] = distFT_MAX;
-			}
-			else
-				distFT[i][j] = RandomIntLessThan(100);
-		}
-	}
-	for (int i = 0; i < pointNumber; i ++)
-	{
-		for (int j = 0; j < i; j ++)
-		{
-			distFT[i][j] = distFT[j][i];
-		}
-	}
-	DisplayMap();
-	Dijkstra(0);
-	Display(0, pointNumber - 1);
-	//object part
+    //Path print out
+    for(int i=0; i<6; i++)
+    {
+        int temp=i;
+        cout<<"path: "<<temp;
+        while(temp!=start)
+        {
+            cout<<" "<<path[temp];
+            temp=path[temp];
+        }
+        cout<<"\t\tcost: "<<cost[i]<<endl;
+    }
+    return 0;
 }
